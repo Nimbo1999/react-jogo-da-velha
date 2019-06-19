7f2ba9c372e68880e998169d643b9dceabd31281
Matheus Lopes <matheus.lopes@presidencia.gov.br>
2019-06-19 17:36:31 -0300

    jogo da velha

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import Game from './components/Game/Game';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      jogador1 : "Player 1",
+      jogador2 : "Player 2",
+    }
+
+    this.jogador1HandleChange = this.jogador1HandleChange.bind(this)
+    this.jogador2HandleChange = this.jogador2HandleChange.bind(this)
+    this.iniciarJogo = this.iniciarJogo.bind(this)
+
+  }
+
+  jogador1HandleChange(event){
+    this.setState({jogador1 : event.target.value})
+  }
+
+  jogador2HandleChange(event){
+    this.setState({jogador2 : event.target.value})
+  }
+
+  iniciarJogo(){
+    ReactDOM.render(
+      <Game nomJogador1={this.state.jogador1} nomJogador2={this.state.jogador2}/>,
+      document.getElementById('root')
+    )
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h2>Jogo da Velha</h2>
+
+        <label htmlFor="jogador1">Nome do jogador 1 (X)</label><br/>
+        <input
+          type="text"
+          value={this.state.jogador1}
+          id="jogador1"
+          size="10"
+          onChange={this.jogador1HandleChange}
+        />
+        <br/><br/>
+
+        <label htmlFor="jogador2">Nome do jogador 2 (O)</label><br/>
+        <input
+          type="text"
+          value={this.state.jogador2}
+          id="jogador2"
+          size="10"
+          onChange={this.jogador2HandleChange}
+        />
+        <br/><br/>
+        <button onClick={this.iniciarJogo}>Confirmar</button>
+        <br/><br/>
+        <div id="Tabuleiro"></div>
+      </div>
+    );
+  }
+}
 export default App;
